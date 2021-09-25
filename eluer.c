8fbb41549fd9aa4fddf72d71a2bdfda9d529ca0c
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-09-24 22:05:22 -0400

    inital commit (euler c file only)

---
+++ eluer.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int multi35(){
+    int total = 0;
+    int i;
+    for (i = 0; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0){
+            total += i;
+        }
+    }
+    return total;
+}
+
+int smallMulti(){
+    int i = 0;
+    int multiple = 3;
+    while (1)
+    {
+        i += 2;
+        for (multiple = 2; multiple <= 20; multiple++)
+        {
+            if (i % multiple != 0){
+                break;
+            }else{
+                if (multiple == 20){
+                    return i;
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    printf("problem 1 %d\n", multi35());
+    printf("problem 5 %d\n", smallMulti());
+    return 0;
+}
